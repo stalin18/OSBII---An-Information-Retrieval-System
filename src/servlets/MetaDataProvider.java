e3c9b7a18ca0f2f34be9a5b50ca710154ae3755f
Kunal Swami <kunalswami@live.in>
2013-08-20 16:57:43 +0530

    Update MetaDataProvider.java

--- src/servlets/MetaDataProvider.java
+++ src/servlets/MetaDataProvider.java
@@ -18,13 +18,13 @@ import classes.CalaisCaller;
 public class MetaDataProvider extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MetaDataProvider() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    	/**
+     	 * @see HttpServlet#HttpServlet()
+     	 */
+	public MetaDataProvider() {
+		super();
+	        // TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
